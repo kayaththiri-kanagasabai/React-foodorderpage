d29338d538850f601b96c81922db45254f2e2e9e
kayaththiri-kanagasabai <kaya282002@gmail.com>
2025-08-01 10:37:49 +0530

    created cart,header,home,placeorder component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Placeorder from './Components/Pages/Placeorder/Placeorder'
+import Cart from './Components/Pages/Cart/Cart'
+import Home from './Components/Pages/Home/Home'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/placeorder' element={<Placeorder/>}/>
+      </Routes>
     </div>
   )
 }
