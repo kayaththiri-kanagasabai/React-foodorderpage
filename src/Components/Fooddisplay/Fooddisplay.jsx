7255c18bee536e669909b763c7b70f8b68f6ba22
kayaththiri-kanagasabai <kaya282002@gmail.com>
2025-08-03 09:13:31 +0530

    created storecontext,fooditem,fooddisplay,exploremenu components created

---
+++ src/Components/Fooddisplay/Fooddisplay.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import './Fooddisplay.css';
+import { StoreContext } from '../../Context/Storecontext';
+import Fooditem from '../Fooditem/Fooditem';
+
+
+const Fooddisplay = (category) => {
+    const {food_list}=useContext(StoreContext);
+
+  return (
+    <div className='food-display' id='food-display'>
+        <h2>Top dishes near you.</h2>
+        <div className="food-display-list">
+            {food_list.map((item,index)=>{
+                return <Fooditem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default Fooddisplay
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css';
 import Header from '../../Header/Header';
+import Exploremenu from '../../Exploremenu/Exploremenu';
+import Fooddisplay from '../../Fooddisplay/Fooddisplay';
 
 const Home = () => {
+    const[category,setCategory]=useState("All");
+
   return (
     <div>
         <Header/>
+        <Exploremenu category={category} setCategory={setCategory}/>
+        <Fooddisplay category={category}/>
     </div>
   )
 }
